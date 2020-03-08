a78cf68949ac52cc6e3e2285a55327ab285a3526
ikane <kaneelhadji2610@yahoo.fr>
2020-03-08 14:54:34 +0100

    first commit

---
+++ src/main/java/com/example/demorabbitmqelastic/DemoRabbitmqElasticApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorabbitmqelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRabbitmqElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRabbitmqElasticApplication.class, args);
+	}
+
+}
